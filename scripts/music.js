704e560df544a10b8ccfa1dc8ad61e12cd2c7517
Ty Busby <tybusby@gmail.com>
2025-05-25 09:22:08 -0400

    sting

--- scripts/music.js
+++ scripts/music.js
@@ -1,7 +1,7 @@
 import { Videos } from "./videos.js";
 const URLS = [
     "https://rumble.com/embed/v6qlj4j/?pub=4",
-    "https://www.youtube.com/embed/iPucwp3uyCE",
+    "https://www.youtube.com/embed/ag6KMH0UlVM",
     "https://www.youtube.com/embed/KHR6HkHySWY",
     "https://www.youtube.com/embed/cRyKCJUXehM",
     "https://www.youtube.com/embed/-xKM3mGt2pE",
