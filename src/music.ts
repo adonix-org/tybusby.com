c7ad6e982edbdb9976aeb242980db18730c0aadb
Ty Busby <tybusby@gmail.com>
2025-05-25 09:26:49 -0400

    doris

--- src/music.ts
+++ src/music.ts
@@ -1,12 +1,14 @@
 import { Videos } from "./videos.js";
 
 const URLS = [
+    "https://www.youtube.com/embed/vmDDOFXSgAs",
     "https://rumble.com/embed/v6qlj4j/?pub=4",
     "https://www.youtube.com/embed/ag6KMH0UlVM",
     "https://www.youtube.com/embed/KHR6HkHySWY",
     "https://www.youtube.com/embed/cRyKCJUXehM",
     "https://www.youtube.com/embed/-xKM3mGt2pE",
     "https://www.youtube.com/embed/4N82TLB03Vk",
+    "https://www.youtube.com/embed/wwXeSk2Helo",
 ];
 
 new Videos("music-video-grid", URLS);
