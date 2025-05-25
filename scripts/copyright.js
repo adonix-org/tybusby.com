ee7a86a7b35106e3a078a71403d3155d227bb50b
Ty Busby <tybusby@gmail.com>
2025-05-25 08:39:02 -0400

    Clean up

--- scripts/copyright.js
+++ scripts/copyright.js
@@ -1,3 +1,2 @@
 export const copyright = `&copy; ${new Date().getFullYear()} tybusby.com`;
-const _copyright = (document.getElementById("copyright").innerHTML =
-    copyright);
+document.getElementById("copyright").innerHTML = copyright;
--- src/copyright.ts
+++ src/copyright.ts
@@ -1,4 +1,3 @@
 export const copyright: string = `&copy; ${new Date().getFullYear()} tybusby.com`;
 
-const _copyright = (document.getElementById("copyright")!.innerHTML =
-    copyright);
+document.getElementById("copyright")!.innerHTML = copyright;
