b2f151292eebf49e23c8525cc70a41681bc1b63d
Ty Busby <tybusby@gmail.com>
2025-05-25 09:03:16 -0400

    checkpoint

--- src/music.ts
+++ src/music.ts
@@ -2,9 +2,10 @@ import { Videos } from "./videos.js";
 
 const URLS = [
     "https://rumble.com/embed/v6qlj4j/?pub=4",
-    "https://www.youtube-nocookie.com/embed/iPucwp3uyCE",
+    "https://www.youtube.com/embed/iPucwp3uyCE",
     "https://www.youtube.com/embed/KHR6HkHySWY",
     "https://www.youtube.com/embed/cRyKCJUXehM",
+    "https://www.youtube.com/embed/-xKM3mGt2pE",
 ];
 
 new Videos("music-video-grid", URLS);
