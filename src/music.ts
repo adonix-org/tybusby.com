07a3b690061f4fe9186bc97a22727e727e7fa8e7
Ty Busby <tybusby@gmail.com>
2025-05-25 11:15:55 -0400

    Judith

--- src/music.ts
+++ src/music.ts
@@ -2,7 +2,7 @@ import { Videos } from "./videos.js";
 
 const URLS = [
     "https://www.youtube.com/embed/vmDDOFXSgAs",
-    "https://rumble.com/embed/v6qlj4j/?pub=4",
+    "https://www.youtube.com/embed/xTgKRCXybSM",
     "https://www.youtube.com/embed/ag6KMH0UlVM",
     "https://www.youtube.com/embed/KHR6HkHySWY",
     "https://www.youtube.com/embed/cRyKCJUXehM",
