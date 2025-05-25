8f9cdd74b3b34029831a0792db9464c61cb171b1
Ty Busby <tybusby@gmail.com>
2025-05-25 08:58:51 -0400

    Remove unembeddable.

--- scripts/music.js
+++ scripts/music.js
@@ -3,5 +3,7 @@ const URLS = [
     "https://rumble.com/embed/v6qlj4j/?pub=4",
     "https://www.youtube-nocookie.com/embed/iPucwp3uyCE",
     "https://www.youtube.com/embed/KHR6HkHySWY",
+    "https://www.youtube.com/embed/cRyKCJUXehM",
+    "https://www.youtube.com/embed/-xKM3mGt2pE",
 ];
 new Videos("music-video-grid", URLS);
--- src/music.ts
+++ src/music.ts
@@ -4,6 +4,7 @@ const URLS = [
     "https://rumble.com/embed/v6qlj4j/?pub=4",
     "https://www.youtube-nocookie.com/embed/iPucwp3uyCE",
     "https://www.youtube.com/embed/KHR6HkHySWY",
+    "https://www.youtube.com/embed/cRyKCJUXehM",
 ];
 
 new Videos("music-video-grid", URLS);
