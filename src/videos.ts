45708b7c1f8a8380f8d41d8bb2431102b87e97c1
Ty Busby <tybusby@gmail.com>
2025-05-26 18:24:26 -0400

    refactor

--- src/videos.ts
+++ src/videos.ts
@@ -9,24 +9,23 @@ export class Videos {
             gridCell.classList.add("grid-cell", "loading");
             _grid.appendChild(gridCell);
 
-            requestAnimationFrame(() => {
-                const video = document.createElement("iframe");
-                video.src = url;
-                video.allow =
-                    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-                video.allowFullscreen = true;
-                video.referrerPolicy = "strict-origin-when-cross-origin";
+            const video = document.createElement("iframe");
+            video.src = url;
+            video.allow =
+                "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+            video.allowFullscreen = true;
+            video.referrerPolicy = "strict-origin-when-cross-origin";
 
-                gridCell.appendChild(video);
+            gridCell.appendChild(video);
 
-                video.addEventListener(
-                    "load",
-                    () => {
-                        gridCell.classList.remove("loading");
-                    },
-                    true
-                );
-            });
+            video.addEventListener(
+                "load",
+                () => {
+                    gridCell.classList.remove("loading");
+                    video.classList.add("loaded");
+                },
+                true
+            );
         });
     }
 }
