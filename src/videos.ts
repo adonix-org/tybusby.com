e8b83f6cbc91503f791ffc81c26dbb4e59b9a46a
Ty Busby <tybusby@gmail.com>
2025-05-26 19:36:48 -0400

    rename video elements

--- src/videos.ts
+++ src/videos.ts
@@ -1,13 +1,13 @@
 export class Videos {
     constructor(
-        private readonly grid: string,
+        private readonly parentId: string,
         private readonly urls: string[]
     ) {
-        const _grid = document.getElementById(grid) as HTMLElement;
+        const parent = document.getElementById(parentId) as HTMLElement;
         urls.forEach((url) => {
-            const gridCell = document.createElement("div");
-            gridCell.classList.add("grid-cell", "loading");
-            _grid.appendChild(gridCell);
+            const videoFrame = document.createElement("div");
+            videoFrame.classList.add("video-frame", "loading");
+            parent.appendChild(videoFrame);
 
             const video = document.createElement("iframe");
             video.src = url;
@@ -16,12 +16,12 @@ export class Videos {
             video.allowFullscreen = true;
             video.referrerPolicy = "strict-origin-when-cross-origin";
 
-            gridCell.appendChild(video);
+            videoFrame.appendChild(video);
 
             video.addEventListener(
                 "load",
                 () => {
-                    gridCell.classList.remove("loading");
+                    videoFrame.classList.remove("loading");
                     video.classList.add("loaded");
                 },
                 true
