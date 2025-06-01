573198c996140d8b9af5476eb48d2e5c8bae9337
Ty Busby <tybusby@gmail.com>
2025-05-31 21:20:32 -0400

    update you tube base url constant

--- src/music.ts
+++ src/music.ts
@@ -1,24 +1,24 @@
 import { VideoGroup } from "./videos.js";
 
-const YOU_TUBE_EMBED = "https://www.youtube-nocookie.com/embed";
+const YT_BASE_URL = "https://www.youtube-nocookie.com/embed";
 
 const URLS = [
-    `${YOU_TUBE_EMBED}/vmDDOFXSgAs`, // Dave Brubeck - Take Five
-    `${YOU_TUBE_EMBED}/xTgKRCXybSM`, // A Perfect Circle - Judith
-    `${YOU_TUBE_EMBED}/ag6KMH0UlVM`, // Sting - I Burn For You
-    `${YOU_TUBE_EMBED}/KHR6HkHySWY`, // Ray Charles - It's Not Easy Being Green
-    `${YOU_TUBE_EMBED}/-xKM3mGt2pE`, // A-Ha - Take On Me (Unplugged)
-    `${YOU_TUBE_EMBED}/4N82TLB03Vk`, // David Gilmour - Fender Guitar 50th Anniversary
-    `${YOU_TUBE_EMBED}/wwXeSk2Helo`, // Doris Day - Autumn Leaves
-    `${YOU_TUBE_EMBED}/Rc7_lCfbQP0`, // Bobby Darin - Beyond The Sea
-    `${YOU_TUBE_EMBED}/2SF1iLXSQto`, // Tom Petty - It's Good To Be King
-    `${YOU_TUBE_EMBED}/8uxt-FnNy2I`, // Coldplay - Don't Panic
-    `${YOU_TUBE_EMBED}/ggGzE5KfCio`, // Ray Charles - Georgia On My Mind
-    `${YOU_TUBE_EMBED}/rPOlakkBlj8`, // Ella Fitzgerald - Misty
-    `${YOU_TUBE_EMBED}/ivdh3zvoQGw`, // Bjork - Heirloom
-    `${YOU_TUBE_EMBED}/PYD-DIggB2k`, // Simon and Garfunkle - April Come She Will
-    `${YOU_TUBE_EMBED}/cRyKCJUXehM`, // Colin Hay - Land Down Under
-    `${YOU_TUBE_EMBED}/DeumyOzKqgI`, // Adele - Skyfall
+    `${YT_BASE_URL}/vmDDOFXSgAs`, // Dave Brubeck - Take Five
+    `${YT_BASE_URL}/xTgKRCXybSM`, // A Perfect Circle - Judith
+    `${YT_BASE_URL}/ag6KMH0UlVM`, // Sting - I Burn For You
+    `${YT_BASE_URL}/KHR6HkHySWY`, // Ray Charles - It's Not Easy Being Green
+    `${YT_BASE_URL}/-xKM3mGt2pE`, // A-Ha - Take On Me (Unplugged)
+    `${YT_BASE_URL}/4N82TLB03Vk`, // David Gilmour - Fender Guitar 50th Anniversary
+    `${YT_BASE_URL}/wwXeSk2Helo`, // Doris Day - Autumn Leaves
+    `${YT_BASE_URL}/Rc7_lCfbQP0`, // Bobby Darin - Beyond The Sea
+    `${YT_BASE_URL}/2SF1iLXSQto`, // Tom Petty - It's Good To Be King
+    `${YT_BASE_URL}/8uxt-FnNy2I`, // Coldplay - Don't Panic
+    `${YT_BASE_URL}/ggGzE5KfCio`, // Ray Charles - Georgia On My Mind
+    `${YT_BASE_URL}/rPOlakkBlj8`, // Ella Fitzgerald - Misty
+    `${YT_BASE_URL}/ivdh3zvoQGw`, // Bjork - Heirloom
+    `${YT_BASE_URL}/PYD-DIggB2k`, // Simon and Garfunkle - April Come She Will
+    `${YT_BASE_URL}/cRyKCJUXehM`, // Colin Hay - Land Down Under
+    `${YT_BASE_URL}/DeumyOzKqgI`, // Adele - Skyfall
 ];
 
 new VideoGroup("music-video-grid", URLS)
