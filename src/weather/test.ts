dda970c3b029164eeb9131bcaf21a1137b58b417
Ty Busby <tybusby@gmail.com>
2025-06-12 18:00:24 -0400

    testing

--- src/weather/test.ts
+++ src/weather/test.ts
@@ -2,8 +2,4 @@ import { Products } from "./products.js";
 
 const hazard = await new Products("HWO", "BGM").get();
 
-if (!hazard) {
-    console.log("Product Not Found");
-} else {
-    console.log(hazard?.id);
-}
+console.log(hazard ?? "Product Not Found");
