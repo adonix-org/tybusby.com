199d15892dc32461565dbd3bd9a56043a5151aa2
Ty Busby <tybusby@gmail.com>
2025-06-12 19:31:39 -0400

    swtich back to BGM

--- src/weather/test.ts
+++ src/weather/test.ts
@@ -1,5 +1,5 @@
 import { Products } from "./products.js";
 
-const hazard = await new Products("HWO", "LWX").get();
+const hazard = await new Products("HWO", "BGM").get();
 
 console.log(hazard ?? "Product Not Found");
