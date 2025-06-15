606cd0e771b6822f0c2c9042d7560dd07dcb4e3f
Ty Busby <tybusby@gmail.com>
2025-06-15 03:47:54 -0400

    add url to JSON error

--- src/weather/nws.ts
+++ src/weather/nws.ts
@@ -39,7 +39,7 @@ export abstract class NationalWeatherService<T> {
         try {
             json = JSON.parse(text);
         } catch (cause) {
-            throw new Error(`NWS JSON Error: "${text}"`, { cause });
+            throw new Error(`NWS JSON Error: ${url} "${text}"`, { cause });
         }
 
         if (!response.ok) {
