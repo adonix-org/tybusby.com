67aecaf0d2fa1ea34fd1b4d7fb261a2258444ff9
Ty Busby <tybusby@gmail.com>
2025-07-28 17:13:29 -0400

    remove use of alert as a variable name

--- src/weather/render/alerts.ts
+++ src/weather/render/alerts.ts
@@ -17,14 +17,14 @@ export class AlertsRender extends BaseRender {
         // Remove exisitng alerts on report refresh.
         alerts.replaceChildren();
 
-        this.report.alerts?.features.forEach((alert) => {
+        this.report.alerts?.features.forEach((feature) => {
             const div = document.createElement("div");
             div.classList.add("alert");
-            div.classList.add(alert.properties.severity.toLowerCase());
-            div.innerText = this.getHeadline(alert);
+            div.classList.add(feature.properties.severity.toLowerCase());
+            div.innerText = this.getHeadline(feature);
 
             const link = document.createElement("a");
-            const product = alert.product;
+            const product = feature.product;
             link.onclick = () => {
                 product?.segments.forEach((segment) => {
                     console.log([product?.headline, segment.body].join("\n\n"));
@@ -35,25 +35,25 @@ export class AlertsRender extends BaseRender {
         });
     }
 
-    private getHeadline(alert: AlertFeature): string {
-        const event = alert.properties.event;
+    private getHeadline(feature: AlertFeature): string {
+        const event = feature.properties.event;
         const timeZone = this.report.point?.properties.timeZone;
 
-        if (!alert.properties.ends) {
+        if (!feature.properties.ends) {
             return event;
         }
 
         const end = formatIsoDate(
-            alert.properties.ends,
+            feature.properties.ends,
             AlertsRender.DATE_TIME_FORMAT,
             timeZone
         );
-        if (isIsoDatePast(alert.properties.onset)) {
+        if (isIsoDatePast(feature.properties.onset)) {
             return `${event} until ${end}`;
         }
 
         const start = formatIsoDate(
-            alert.properties.onset,
+            feature.properties.onset,
             AlertsRender.DATE_TIME_FORMAT,
             timeZone
         );
