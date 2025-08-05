281c46f6faba9137f483bf177be2dc304a95b011
Ty Busby <tybusby@gmail.com>
2025-08-04 23:40:17 -0400

    remove parser

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
                 weather: resolve(__dirname, "weather.html"),
                 music: resolve(__dirname, "music.html"),
                 podcast: resolve(__dirname, "podcast.html"),
-                parser: resolve(__dirname, "car_talk.html"),
             },
         },
     },
