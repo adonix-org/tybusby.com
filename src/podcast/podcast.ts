202e89e2ea3f44319a0f47237d9063ef2dead060
Ty Busby <tybusby@gmail.com>
2025-08-07 19:03:59 -0400

    initial

--- src/podcast/podcast.ts
+++ src/podcast/podcast.ts
@@ -42,19 +42,24 @@ export class HTTPError extends Error {
 }
 
 export class Podcast {
-    private static readonly BASE = new URL("https://podcast.adonix.org");
+    private static readonly VERSION = "v1";
+
+    private static readonly BASE = `https://cartalk.adonix.org`;
+    private static readonly API = new URL(
+        `${Podcast.BASE}/api/${Podcast.VERSION}/seasons`
+    );
 
     public async getSeasons(): Promise<string[]> {
         return await this.get<string[]>();
     }
 
     public async getPlaylist(season: string): Promise<MetaData[]> {
-        const url = new URL(Podcast.BASE.toString());
-        url.searchParams.set("season", season);
+        const url = new URL(`${Podcast.API.toString()}/${season}`);
+        console.log(url.pathname);
         return await this.get<MetaData[]>(url);
     }
 
-    protected async get<T>(url: URL = Podcast.BASE): Promise<T> {
+    protected async get<T>(url: URL = Podcast.API): Promise<T> {
         let response: Response;
         try {
             response = await fetch(url, {
