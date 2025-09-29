31e5618de0b88ad2f59d8757e7c718074e80bd47
Ty Busby <tybusby@gmail.com>
2025-09-28 21:31:56 -0400

    fix url

--- src/podcast/track.ts
+++ src/podcast/track.ts
@@ -106,7 +106,7 @@ export class Track {
             artist: this.data.artist,
             artwork: [
                 {
-                    src: "https://audio.adonix.org/artwork/cover.jpg",
+                    src: `${Podcast.BASE_URL}/artwork/cover.jpg`,
                     sizes: "512x512",
                     type: "image/jpeg",
                 },
