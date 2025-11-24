77e54195ae687fed2dedeb934848dbc15ecf7bfd
Ty Busby <tybusby@gmail.com>
2025-11-24 12:57:21 -0500

    temp logging of 404 response

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -4,7 +4,13 @@ class Intercept extends BasicWorker {
     protected override async get(): Promise<Response> {
         const response = await this.env.ASSETS.fetch(this.request);
         if (response.status === StatusCodes.NOT_FOUND) {
-            console.log(response);
+            console.log({
+                url: response.url,
+                status: response.status,
+                statusText: response.statusText,
+                ok: response.ok,
+                headers: Object.fromEntries([...response.headers]),
+            });
             return this.response(NotFound);
         }
         return response;
